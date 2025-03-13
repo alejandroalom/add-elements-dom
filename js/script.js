f95751ed7a7edf92adb3d5d2d983d7f04871e87f
Alejandro Alom <alejandroalom@gmail.com>
2025-03-13 16:47:18 +0000

    Eliminado submódulo add-elements-dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
  // Aquí tu código
+
+const botonAgregar = document.getElementById('agregar'); 
+const lista = document.getElementById('lista'); 
+
+
+botonAgregar.addEventListener('click', function() {
+  const nuevoElemento = prompt("Introduce un elemento para la lista:");
+  if (nuevoElemento === null || nuevoElemento.trim() === '') {
+    return; 
+  }
+  const li = document.createElement('li'); 
+  li.textContent = nuevoElemento; 
+
+  lista.appendChild(li);
+});
